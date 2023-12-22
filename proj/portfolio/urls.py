e22102abfeac66702a4fb6cc24cf71755ca9dd93
shiv <shivshankarchaudharyex14@gmail.com>
2023-12-22 17:21:53 +0530

    'initially'

---
+++ proj/portfolio/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from portfolio import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('blog',views.handleblog,name='handleblog'),
+    path('internshipdetails',views.internshipdetails,name='internshipdetails'),
+    # path('show_file',views.show_file,name='show_file'),
+    
+]
+
+
+
+
